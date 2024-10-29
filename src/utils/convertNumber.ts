6a4b281a9ed94849b9aaebe1e11a0ac434ea53a6
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-10-29 21:21:04 +0900

    feat: formatCard function

--- src/utils/convertNumber.ts
+++ src/utils/convertNumber.ts
@@ -1,17 +1,28 @@
 import { values, suites, Card } from "../types/type" 
 
 const convertNumber = (value: number) => {
-  if (value == 1) return "A"
-  if (value <= 10) return value.toString()
-  if (value == 11) return "J"
-  if (value == 12) return "Q"
-  if (value == 13) return "K"
+  if (value == 1) return "A";
+  if (value <= 10) return value.toString();
+  if (value == 11) return "J";
+  if (value == 12) return "Q";
+  if (value == 13) return "K";
 }
 
-const generateDeck = (): Card[] => 
-  suites.flatMap(suite => 
+const generateDeck = (): Card[] => {
+  return suites.flatMap(suite => 
     values.map(value => ({
       value,
       suite
     }))
-  )
+  );
+}
+
+const formatCard = ({value, suite}: Card): string => {
+  return convertNumber(value) + " " + suite;
+}
+
+export {
+  convertNumber,
+  generateDeck,
+  formatCard
+}
