4fc7fa3971af3b5607b20d661d12952811343bc5
Adrian <adrian.hamzah15@gmail.com>
2024-10-30 22:55:10 +0700

    feat: create utility function generateHand

---
+++ src/utils/generateHandAndBoard.ts
@@ -0,0 +1,17 @@
+import { Card } from "../types/type";
+import { generateDeck } from "./convertNumber";
+
+const generateHand = (playerCount: number) => (deck: Card[]): [Card[][], Card[]] => {
+  const hands = Array.from({ length: playerCount }, (_, player) => 
+    [deck[player * 2], deck[player * 2 + 1]]
+  );
+
+  const remainingDeck = deck.reduce((acc: Card[], card, index) => {
+    return index >= playerCount * 2 ? [...acc, card] : acc;
+  }, []);
+
+  return [hands, remainingDeck];
+};
+
+const deck = generateDeck();
+console.log(generateHand(5)(deck))
