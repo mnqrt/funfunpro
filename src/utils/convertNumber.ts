ac0b9938e1e17609c408611e0195a5edea8d8731
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-10-31 00:17:15 +0900

    feat: unformatCard and unconvertNumber

--- src/utils/convertNumber.ts
+++ src/utils/convertNumber.ts
@@ -1,11 +1,19 @@
-import { values, suites, Card } from "../types/type" 
+import { values, suites, Card, Value, Suite } from "../types/type" 
 
-const convertNumber = (value: number) => {
+const convertNumber = (value: Value): string => {
   if (value == 1) return "A";
-  if (value <= 10) return value.toString();
   if (value == 11) return "J";
   if (value == 12) return "Q";
   if (value == 13) return "K";
+  return value.toString();
+}
+
+const unconvertNumber = (value: string): Value => {
+  if (value == "A") return 1;
+  if (value == "J") return 11;
+  if (value == "Q") return 12;
+  if (value == "K") return 13;
+  return parseInt(value) as Value;
 }
 
 const generateDeck = (): Card[] => {
@@ -21,8 +29,17 @@ const formatCard = ({value, suite}: Card): string => {
   return convertNumber(value) + " " + suite;
 }
 
+const unformatCard = (card: string): Card => {
+  const [cardVal, cardSuite] = card.split(' ');
+  return {
+    suite: cardSuite as Suite,
+    value: unconvertNumber(cardVal)
+  }
+}
+
 export {
   convertNumber,
   generateDeck,
-  formatCard
+  formatCard,
+  unformatCard
 }
