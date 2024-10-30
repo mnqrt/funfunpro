762c6022f45801f65043df49a469fb53727045e0
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-10-31 00:17:53 +0900

    fix: use previous state in PRNG so it is deterministic given a seed

--- src/utils/generateRandom.ts
+++ src/utils/generateRandom.ts
@@ -6,76 +6,45 @@ import { getCurrentTime, getTommorow } from "./getTime";
 type RandState = bigint;
 type Xorshift64State = RandState;
 
-const deterministicSeed = (seed: number = 1): Xorshift64State => BigInt(seed);
+const deterministicSeed = (seed: number = 1): RandState => BigInt(seed);
 
-const xorshift64Step = (state: Xorshift64State): Xorshift64State => {
-  let x = state;
-  x ^= x << BigInt(13);
-  x ^= x >> BigInt(7);
-  x ^= x << BigInt(17);
-  return x;
-}
-
-const next = (state: Xorshift64State): [bigint, Xorshift64State] => {
-  const newState = xorshift64Step(state);
-  return [newState, newState];
+const nextXorshift64 = (state: Xorshift64State): Xorshift64State => {
+  const a = state ^ (state << BigInt(13));
+  const b = a ^ (a >> BigInt(7));
+  return b ^ (b << BigInt(17));
 }
 
 const generateRandomList = (
-  generator: (state: RandState) => [bigint, RandState],
+  generator: (state: RandState) => RandState,
   state: RandState,
   count: number,
   transform: (value: bigint) => bigint = (value) => value
 ): bigint[] => {
   if (count == 0) return [];
-  const [val, nextState] = generator(state);
-  return [transform(val), ...generateRandomList(generator, nextState, count-1, transform)]
-
-  // let sequence = [];
-  // let state = initialState;
-  // for (let i = 0; i < count; i++) {
-  //   const [randomValue, newState] = generator(state);
-  //   sequence.push(transform(randomValue))
-  //   state = newState;
-  // }
-
-  // return sequence;
+  const nextState = generator(state);
+  return [transform(nextState), ...generateRandomList(generator, nextState, count-1, transform)]
 }
-
-// const seed = deterministicSeed(42);
-
-// const randomSequence = generateRandomList(next, seed, 60);
 
 const moduloBy = (value: bigint, modulus: number): bigint => value % BigInt(modulus)
 
-const shuffleDeck = (deck: Card[], state: () => number): Card[] => {
+const shuffleDeck = (
+  deck: Card[],
+  rngGen: (state: RandState) => RandState = nextXorshift64,
+  state: RandState = deterministicSeed(getCurrentTime())
+): Card[] => {
   const n = deck.length;
   if (n == 0) return [];
 
-  const [idx, _] = next(deterministicSeed(state()));
-  const el = deck[Number(moduloBy(idx, n))];
-  return [el, ...shuffleDeck(deck.filter(isNotEqual(el)), state)];
+  const nextState = rngGen(state);
+  const el = deck[Number(moduloBy(nextState, n))];
+  return [el, ...shuffleDeck(deck.filter(isNotEqual(el)), rngGen, nextState)];
 }
 
 const deck = generateDeck();
 console.log("Deck: ", deck.map(formatCard));
-console.log("Shuffled: ", shuffleDeck(deck, getTommorow).map(formatCard))
-//                       Note: can also use getCurrentTime
+console.log("Shuffled: ", shuffleDeck(deck, nextXorshift64, deterministicSeed(1)).map(formatCard))
+// Note: can also use getCurrentTime
 
-// console.log("Random sequence of 64-bit numbers:", randomSequence.map(n => moduloBy(n, 13).toString()));
-
-// const doubledSequence = generateCustomSequence(
-//   next, 
-//   seed, 
-//   5, 
-//   (value) => value * BigInt(2)
-// );
-// console.log("Doubled random sequence of 64-bit numbers:", doubledSequence.map(n => n.toString()));
-
-// const squaredSequence = generateCustomSequence(
-//   next,
-//   seed,
-//   5,
-//   (value) => value * value
-// );
-// console.log("Squared random sequence of 64-bit numbers:", squaredSequence.map(n => n.toString()));
+export {
+  shuffleDeck,
+}
