f65e9ff05f840fcc329004f2b46ff6c7b2edabb9
Adrian <adrian.hamzah15@gmail.com>
2024-11-12 16:07:48 +0700

    feat: update pipe function to support multiple function types

--- src/utils/combine.ts
+++ src/utils/combine.ts
@@ -1,4 +1,7 @@
-const pipe = <T>(...fns: ((val: T) => T)[]) => (x: T) => fns.reduce((v, f) => f(v), x);
+const pipe = <T, R>(...fns: [(x: T) => any, ...Array<(x: any) => any>]): (x: T) => R => {
+  return (x: T) => fns.reduce((value, fn) => fn(value), x) as R;
+};
+
 const map = <T>(fn: ((val: T) => T)) => (arr: T[]) => arr.map(fn);
 const sort = <T>(compareFn: (a: T, b: T) => number) => (arr: T[]) => [...arr].sort(compareFn);
 
