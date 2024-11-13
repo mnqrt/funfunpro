a52f2c4cdb62ceebecd831da587116494ce75e19
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-11-13 23:29:44 +0900

    feat: API endpoints for poker

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { generateDeck, formatCard, unformatCard } from './utils/convert';
 import { shuffleDeck } from './utils/generateRandom';
+import { generateHand, generateBoard } from "./utils/generateHandAndBoard";
+import { compareHandsFactory } from './utils/compareHand';
+
 const app = express();
 const port = 3000;
 
@@ -13,7 +16,28 @@ app.post('/generate', (req, res) => {
 });
 
 app.patch('/shuffle', (req, res) => {
-  res.send(JSON.stringify(shuffleDeck(JSON.parse(req.body).map(unformatCard))))
+  res.send(JSON.stringify(shuffleDeck(JSON.parse(req.body).map(unformatCard)).map(formatCard)))
+});
+
+app.post('/generate/hand', (req, res) => {
+  const body = JSON.parse(req.body);
+  const players = body.players;
+  const deck = body.deck.map(unformatCard);
+  const [hands, rest] = generateHand(players)(deck);
+  res.send(JSON.stringify({hands: hands.map(hand => hand.map(formatCard)), deck: rest.map(formatCard)}));
+});
+
+app.post('/generate/board', (req, res) => {
+  const body = JSON.parse(req.body);
+  const deck = body.deck.map(unformatCard);
+  const [board, rest] = generateBoard(deck);
+  res.send(JSON.stringify({board: board.map(formatCard), deck: rest.map(formatCard)}));
+});
+
+app.post('/compare', (req, res) => {
+  const body = JSON.parse(req.body);
+  const { hand1, hand2, board } = body;
+  res.send(JSON.stringify({hand1: hand1, hand2: hand2, board: board, winner: compareHandsFactory(board)(hand1, hand2)}));
 });
 
 app.listen(port, () => {
